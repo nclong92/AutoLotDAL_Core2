fc0dc3a06058bc7c5bac20e73a0d848d939c8fd1
longnc <nclong92@gmail.com>
2019-06-27 17:31:03 +0700

    First commit

---
+++ AutoLotDAL_Core2/EF/AutoLotContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoLotDAL_Core2.EF
+{
+    public class AutoLotContextFactory : IDesignTimeDbContextFactory<AutoLotContext>
+    {
+        public AutoLotContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AutoLotContext>();
+            var connectionString = @"server=(localdb)\MSSQLLocalDB;database=AutoLotCore2;
+                integrated security=True; MultipleActiveResultSets=True;App=EntityFramework;";
+
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId
+                .QueryClientEvaluationWarning));
+
+            return new AutoLotContext(optionsBuilder.Options);
+        }
+    }
+}
