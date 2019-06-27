a73e9a161435fb72c31e6e939e1f8643e0e3791d
longnc <nclong92@gmail.com>
2019-06-27 14:09:28 +0700

    Add project files.

---
+++ AutoLotDAL_Core2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutoLotDAL_Core2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
